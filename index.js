01c64f4b841bcfe812060c55ef537cf143bee486
dulangd <79493202+dulangd@users.noreply.github.com>
2026-09-20 11:04:50 +0800

    Align runtime version with 1.1.2

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const https = require('https');
 const net = require('net');
 const crypto = require('crypto');
 
-const VERSION = '1.1.1';
+const VERSION = '1.1.2';
 const PROVIDER = 'infrlo';
 const HOST = '0.0.0.0';
 const PORT = validPort(process.env.PORT) || 8080;
